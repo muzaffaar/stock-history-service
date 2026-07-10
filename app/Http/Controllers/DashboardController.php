9bb5dbee0724a49601b415d1f0fe888205a08028
Muzaffar Tursunov <muzaffar-shoshiy@mailbox.unideb.hu>
2026-07-10 07:17:30 +0200

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,7 +13,7 @@ class DashboardController extends Controller
 
     public function index()
     {
-        return view('dashboard.index', [
+        return view('dashboard.test', [
             'data' => $this->dashboard->data()
         ]);
     }
